a6949bd2f83831ca872228090542c4c7586b9fd0
gencalda <gencalda@gmail.com>
2024-12-20 09:57:01 +0800

    completed next-js tutorial

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  experimental: {
+    ppr: "incremental",
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
